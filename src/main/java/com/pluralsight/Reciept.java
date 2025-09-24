0abecefda15b7bd2606a04bd5ead44b1d749801a
wasidc <wasid.chowdhury@icloud.com>
2025-09-24 13:58:37 -0400

    Initial Commit

--- src/main/java/com/pluralsight/Reciept.java
+++ src/main/java/com/pluralsight/Reciept.java
@@ -1,4 +1,14 @@
 package com.pluralsight;
 
 public class Reciept {
+    public static void main(String[] args) {
+        String itemName = "Apple";
+        double itemPrice = 2.37;
+        int quantity = 3;
+
+        double totalPrice = itemPrice * quantity;
+
+        System.out.println("You bought " + quantity + " " + itemName + " for $" + totalPrice);
+
+    }
 }
--- src/main/java/com/pluralsight/VariableApp.java
+++ src/main/java/com/pluralsight/VariableApp.java
@@ -1,4 +1,35 @@
 package com.pluralsight;
 
 public class VariableApp {
+    public static void main(String[] args) {
+
+        String favoriteColor = "Blue";
+        System.out.println("My favorite color is " + favoriteColor);
+
+        int yearStartedClass = 2025;
+        System.out.println("I started this class in the year " + yearStartedClass);
+
+        // If I had middle initial would use char
+        String middleInitial = "NA";
+        System.out.println("My middle initial is " + middleInitial);
+
+        boolean hasPets = true;
+        System.out.println("Do I have pets? " + (hasPets));
+
+        String niceMessage = "Never give up!";
+        System.out.println("Here is a nice message: " + niceMessage);
+
+        int daysInWeek = 7;
+        System.out.println("There is " + daysInWeek + "days in a week.");
+
+        double coffeePrice = 4.99;
+        System.out.println("The price of coffee is $" + coffeePrice);
+
+        char favoriteLetter = 'W';
+        System.out.println("My favorite letter is " + favoriteLetter);
+
+        boolean isRaining = false;
+        System.out.println("Is it raining? " + (isRaining));
+
+    }
 }
